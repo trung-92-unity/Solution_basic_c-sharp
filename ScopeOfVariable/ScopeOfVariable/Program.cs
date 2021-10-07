8dc87c234adc843881caf29e97ebaca9ab8538df
trungtd <tranductrung.nga.lhp.cabal@gmail.com>
2021-10-07 13:26:15 +0700

    no message

---
+++ ScopeOfVariable/ScopeOfVariable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ScopeOfVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            Console.WriteLine(i);
+            {
+                var j = 'a';
+                Console.WriteLine(j);
+                Console.WriteLine("khoi code ben trong " + i);
+            }
+            
+        }
+    }
+}
